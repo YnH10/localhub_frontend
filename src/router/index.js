dcfc9115910bb30c28788609a4f5d13b3ed8598c
YOUNHWAN <choiyounhwan@hanyang.ac.kr>
2026-07-15 03:05:28 +0900

    Edited index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,40 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeViews.vue'),
+    },
+    {
+      path: '/posts',
+      name: 'post-list',
+      component: () => import('../views/PostListView.vue'),
+    },
+    {
+      path: '/posts/new',
+      name: 'post-create',
+      component: () => import('../views/PostFormatView.vue'),
+    },
+    {
+      path: '/posts/:id',
+      name: 'post-detail',
+      component: () => import('../views/PostDetailView.vue'),
+      props: true,
+    },
+    {
+      path: '/posts/:id/edit',
+      name: 'post-edit',
+      component: () => import('../views/PostFormatView.vue'),
+      props: true,
+    },
+    {
+      path: '/rocket',
+      name: 'rocket-dashboard',
+      component: () => import('../views/RocketDashboardView.vue'),
+    },
+  ],
 })
 
 export default router
